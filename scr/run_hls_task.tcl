cec614ca957418cac25b84b35ab1e9ddcc9a4508
Sheshu Kalaparmbathu Ramanandan <Sheshu.Ramanandan@smartray.com>
2020-09-20 13:27:21 +0200

    [fir] updated infrastructure to make multiples solutions at once

--- scr/run_hls_task.tcl
+++ scr/run_hls_task.tcl
@@ -3,10 +3,13 @@
 #------------------------------------------------------------------
 
 set   PROJECT_NAME				      $::env(PROJECT_NAME)
+set   DEV_PARTNAME            	$::env(DEV_PARTNAME)
 set   SOLUTION_NAME           	$::env(SOLUTION_NAME)
 
 set   HLS_EXEC                	$::env(HLS_EXEC)
 
+set	  TARGET_CLOCK_PERIOD_NS  	$::env(TARGET_CLOCK_PERIOD_NS)
+set	  DIRECTIVE_TCL_FILE      	$::env(DIRECTIVE_TCL_FILE)
 
 #------------------------------------------------------------------
 # Creating the Project
@@ -19,6 +22,11 @@ open_project  		              ${PROJECT_NAME}
 #------------------------------------------------------------------
 
 open_solution				            ${SOLUTION_NAME}
+set_part					              ${DEV_PARTNAME}
+
+create_clock	-period		        ${TARGET_CLOCK_PERIOD_NS}
+
+source						              ${DIRECTIVE_TCL_FILE}
 
 #------------------------------------------------------------------
 # Run Simulate
