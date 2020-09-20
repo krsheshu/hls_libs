98602fccec25719fa66e4478429f50fe2677a1e9
Sheshu Kalaparmbathu Ramanandan <Sheshu.Ramanandan@smartray.com>
2020-09-20 13:31:38 +0200

    [fir] retabbed tcl files

--- scr/config_hls_project.tcl
+++ scr/config_hls_project.tcl
@@ -2,31 +2,31 @@
 # Setting project paths and output directory
 #------------------------------------------------------------------
 
-set   PROJECT_NAME				      $::env(PROJECT_NAME)
+set   PROJECT_NAME              $::env(PROJECT_NAME)
 
-set   SRC_FILES               	$::env(SRC_FILES)
-set	  TOP_LEVEL_FUNCTION      	$::env(TOP_LEVEL_FUNCTION)
+set   SRC_FILES                 $::env(SRC_FILES)
+set   TOP_LEVEL_FUNCTION        $::env(TOP_LEVEL_FUNCTION)
 
-set   TB_FILES				  	      $::env(TB_FILES)
+set   TB_FILES                  $::env(TB_FILES)
 
 #------------------------------------------------------------------
 # Creating the Project
 #------------------------------------------------------------------
 
-open_project  -reset		        ${PROJECT_NAME}
+open_project  -reset            ${PROJECT_NAME}
 
 #------------------------------------------------------------------
 # Add hls design files and set top level function
 #------------------------------------------------------------------
 
-add_files					              [ glob ${SRC_FILES}	]
-set_top						              ${TOP_LEVEL_FUNCTION}
+add_files                       [ glob ${SRC_FILES} ]
+set_top                         ${TOP_LEVEL_FUNCTION}
 
 #------------------------------------------------------------------
 # Add testbench files
 #------------------------------------------------------------------
 
-add_files		-tb                 [ glob ${TB_FILES}	]
+add_files   -tb                 [ glob ${TB_FILES}  ]
 
 #------------------------------------------------------------------
 # Done!
--- scr/run_hls_task.tcl
+++ scr/run_hls_task.tcl
@@ -2,37 +2,37 @@
 # Setting project paths and output directory
 #------------------------------------------------------------------
 
-set   PROJECT_NAME				      $::env(PROJECT_NAME)
-set   DEV_PARTNAME            	$::env(DEV_PARTNAME)
-set   SOLUTION_NAME           	$::env(SOLUTION_NAME)
+set   PROJECT_NAME              $::env(PROJECT_NAME)
+set   DEV_PARTNAME              $::env(DEV_PARTNAME)
+set   SOLUTION_NAME             $::env(SOLUTION_NAME)
 
-set   HLS_EXEC                	$::env(HLS_EXEC)
+set   HLS_EXEC                  $::env(HLS_EXEC)
 
-set	  TARGET_CLOCK_PERIOD_NS  	$::env(TARGET_CLOCK_PERIOD_NS)
-set	  DIRECTIVE_TCL_FILE      	$::env(DIRECTIVE_TCL_FILE)
+set   TARGET_CLOCK_PERIOD_NS    $::env(TARGET_CLOCK_PERIOD_NS)
+set   DIRECTIVE_TCL_FILE        $::env(DIRECTIVE_TCL_FILE)
 
 #------------------------------------------------------------------
 # Creating the Project
 #------------------------------------------------------------------
 
-open_project  		              ${PROJECT_NAME}
+open_project                    ${PROJECT_NAME}
 
 #------------------------------------------------------------------
 # Open and Configure the Solution, apply directives
 #------------------------------------------------------------------
 
-open_solution				            ${SOLUTION_NAME}
-set_part					              ${DEV_PARTNAME}
+open_solution                   ${SOLUTION_NAME}
+set_part                        ${DEV_PARTNAME}
 
-create_clock	-period		        ${TARGET_CLOCK_PERIOD_NS}
+create_clock  -period           ${TARGET_CLOCK_PERIOD_NS}
 
-source						              ${DIRECTIVE_TCL_FILE}
+source                          ${DIRECTIVE_TCL_FILE}
 
 #------------------------------------------------------------------
 # Run Simulate
 #------------------------------------------------------------------
 if { ${HLS_EXEC} == 0 } {
-	csim_design
+  csim_design
 }
 
 #------------------------------------------------------------------
